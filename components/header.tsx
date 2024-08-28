f3fcad5fcebe4b3b245f3bcca844b58ff0af3139
Lucas-Rodrigues-code <Rodriguesdesouzal28@gmail.com>
2024-08-28 14:32:29 -0300

    refactor: header

--- components/header.tsx
+++ components/header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
           <CompassIcon className="w-6 h-6" />
           <span className="text-2xl font-bold">TripAdvisor</span>
         </Link>
-        <div className="flex items-center gap-4">
+        <div className="md:flex items-center gap-4 hidden">
           <Link
             href="#"
             className="text-sm font-medium hover:underline"
@@ -35,27 +35,46 @@ export default function Header() {
             Sign In
           </Button>
         </div>
+        <div className="md:hidden gap-4">
+          <Menu />
+        </div>
       </div>
     </header>
   );
 }
 
-function CompassIcon(props:any) {
-    return (
-      <svg
-        {...props}
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="m16.24 7.76-1.804 5.411a2 2 0 0 1-1.265 1.265L7.76 16.24l1.804-5.411a2 2 0 0 1 1.265-1.265z" />
-        <circle cx="12" cy="12" r="10" />
-      </svg>
-    )
-  }
+function CompassIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m16.24 7.76-1.804 5.411a2 2 0 0 1-1.265 1.265L7.76 16.24l1.804-5.411a2 2 0 0 1 1.265-1.265z" />
+      <circle cx="12" cy="12" r="10" />
+    </svg>
+  );
+}
+
+function Menu(props: any) {
+  return (
+    <svg
+      {...props}
+      width="24"
+      height="24"
+      viewBox="0 0 100 80"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="100" height="10" rx="5" fill="white" />
+      <rect y="30" width="100" height="10" rx="5" fill="white" />
+      <rect y="60" width="100" height="10" rx="5" fill="white" />
+    </svg>
+  );
+}
